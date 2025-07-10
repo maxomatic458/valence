ee071c7ba039d86c43fe74a1af7536df8b0e6f69
maxomatic458 <104733404+maxomatic458@users.noreply.github.com>
2025-07-10 18:14:27 +0200

    add initial support for `ChatCommandSignedC2s`

--- crates/valence_command/src/manager.rs
+++ crates/valence_command/src/manager.rs
@@ -13,7 +13,7 @@ use tracing::{debug, info, trace, warn};
 use valence_server::client::{Client, SpawnClientsSet};
 use valence_server::event_loop::PacketEvent;
 use valence_server::protocol::packets::play::commands_s2c::NodeData;
-use valence_server::protocol::packets::play::{ChatCommandSignedC2s, CommandsS2c};
+use valence_server::protocol::packets::play::{ChatCommandC2s, ChatCommandSignedC2s, CommandsS2c};
 use valence_server::protocol::WritePacket;
 use valence_server::EventLoopPreUpdate;
 
@@ -92,9 +92,19 @@ fn read_incoming_packets(
 ) {
     for packet in packets.read() {
         let client = packet.client;
-        if let Some(packet) = packet.decode::<ChatCommandSignedC2s>() {
+
+        if let Some(unsigned) = packet.decode::<ChatCommandC2s>() {
             event_writer.send(CommandExecutionEvent {
-                command: packet.command.to_string(),
+                command: unsigned.command.to_string(),
+                executor: client,
+            });
+        } else if let Some(signed) = packet.decode::<ChatCommandSignedC2s>() {
+            // TODO: verify command signatures.
+            // As per this gist: https://gist.github.com/kennytv/ed783dd244ca0321bbd882c347892874
+            // It looks like the client only sends the signed version if a command requires it (in
+            // vanilla thats /say for example).
+            event_writer.send(CommandExecutionEvent {
+                command: signed.command.to_string(),
                 executor: client,
             });
         }
--- crates/valence_protocol/src/packets/play/chat_command_signed_c2s.rs
+++ crates/valence_protocol/src/packets/play/chat_command_signed_c2s.rs
@@ -1,20 +1,21 @@
-use crate::{Bounded, Decode, Encode, FixedBitSet, Packet, VarInt};
+use crate::{Bounded, Decode, Encode, FixedBitSet, Packet, VarInt, RawBytes};
 
 #[derive(Clone, Debug, Encode, Decode, Packet)]
 pub struct ChatCommandSignedC2s<'a> {
     pub command: Bounded<&'a str, 256>,
     pub timestamp: u64,
     pub salt: u64,
-    pub argument_signatures: Vec<CommandArgumentSignature<'a>>,
+    pub argument_signatures: Bounded<Vec<CommandArgumentSignature<'a>>, 8>,
     pub message_count: VarInt,
     //// This is a bitset of 20; each bit represents one
     //// of the last 20 messages received and whether or not
     //// the message was acknowledged by the client
     pub acknowledgement: FixedBitSet<20, 3>,
+    pub checksum: u8,
 }
 
 #[derive(Copy, Clone, Debug, Encode, Decode)]
 pub struct CommandArgumentSignature<'a> {
     pub argument_name: Bounded<&'a str, 16>,
-    pub signature: &'a [u8; 256],
+    pub signature: Bounded<RawBytes<'a>, 256>,
 }
